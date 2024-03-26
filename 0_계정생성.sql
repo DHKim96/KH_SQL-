53211080485f7db3a42c92c02fe0aef12dd8e286
DHKim96 <history8549@gmail.com>
2024-03-26 18:47:25 +0900

    인잇

--- 0_계정생성.sql
+++ 0_계정생성.sql
@@ -9,8 +9,8 @@ SELECT * FROM DBA_USERS; -- 현재 모든 계정들에 대해서 조회하는 명령문
 -- 일반 사용자 계정을 생성하는 구문(오직 관리자 계정에서만 할 수 있음)
 -- [표현법] CREATE USER 계정명 IDENTIFIED BY 비밀번호;
 -- (단, 비밀번호는 대소문자 구분함)
-CREATE USER KH IDENTIFIED BY KH;
+CREATE USER SERVER IDENTIFIED BY SERVER;
 
 -- 위에서 생성된 일반 사용자 계정에 최소한의 권한(접속, 데이터 관리) 부여
 -- [표현법] GRANT 권한1, 권한2 ... TO 계정명;
-GRANT RESOURCE, CONNECT TO KH;
+GRANT RESOURCE, CONNECT TO SERVER;
